3712222ab72b89658bcd1a670b006fdc4954a989
Saif Hesham <saifeldeen.hesham@gmial.com>
2025-09-24 16:22:03 +0300

    Refactor makePill.js for consistent formatting and style

--- src/js/factory/makePill.js
+++ src/js/factory/makePill.js
@@ -2,28 +2,29 @@ import Animation, { ANIMATION_VERTICAL } from '../engine/Animation.js';
 import Item from '../Item.js';
 
 const animationBase = {
-    imageURL : 'img/heart-hi-dark.svg',
-    numberOfFrame : 2,
-    delta : 48,
-    refreshRate : 450,
-    type : ANIMATION_VERTICAL
+  imageURL: 'img/heart-hi-dark.svg',
+  numberOfFrame: 2,
+  delta: 48,
+  refreshRate: 450,
+  type: ANIMATION_VERTICAL,
 };
 
-export default (options) => new Item({
-    width : 48,
-    height : 48,
-    animations : {
-        'white' : new Animation({
-            ...animationBase
-        }),
-        'yellow' : new Animation({
-            ...animationBase,
-            offsetX : 24 + 8
-        }),
-        'red' : new Animation({
-            ...animationBase,
-            offsetX : (24 + 8) * 2
-        })
+export default options =>
+  new Item({
+    width: 48,
+    height: 48,
+    animations: {
+      white: new Animation({
+        ...animationBase,
+      }),
+      yellow: new Animation({
+        ...animationBase,
+        offsetX: 24 + 8,
+      }),
+      red: new Animation({
+        ...animationBase,
+        offsetX: (24 + 8) * 2,
+      }),
     },
-    ...options
-});
+    ...options,
+  });
